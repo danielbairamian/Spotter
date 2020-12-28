d9253d9029531d8330c40376e53cf36e937ab6b2
Daniel Bairamian <daniel.bairamian@mail.mcgill.ca>
2020-12-28 17:06:13 -0500

    reward history and caching render frames

--- src/spotter.py
+++ src/spotter.py
@@ -2,6 +2,7 @@ import copy
 
 '''Helper class that will hold per-agent information'''
 
+
 def disable_view_window():
     from gym.envs.classic_control import rendering
     org_constructor = rendering.Viewer.__init__
@@ -15,16 +16,17 @@ def disable_view_window():
 
 
 class Spotter_Agent:
-    def __init__(self, agent, function_handle, env, init_obs):
-        """disable view window moves renderes outside canvas so we don't see them
+    def __init__(self, agent, function_handle):
+        """disable view window moves renders outside canvas so we don't see them
             There might be a better way to do this"""
-        disable_view_window()
         self.agent = agent
         self.function_handle = function_handle
-        self.env = env
-        self.obs = init_obs
+        self.ep_reward_hist = []
         self.ep_reward = 0
         self.is_done_ep = False
+        self.env = None
+        self.obs = None
+        self.last_frame = None
 
     """sample action according to given function handle"""
     def take_action(self, obs):
@@ -43,15 +45,24 @@ class Spotter:
         self.spotter_agents = []
         self.agent_list = agent_list
         self.function_handle_list = function_handle_list
+        self.create_agents()
+        disable_view_window()
+
+    """create Spotter agents"""
+    def create_agents(self):
+        for i in range(len(self.agent_list)):
+            spotter_agent = Spotter_Agent(self.agent_list[i], self.function_handle_list[i])
+            self.spotter_agents.append(spotter_agent)
 
     """reset agents for every episode"""
     def reset_agents(self):
         init_obs = self.env.reset()
-        self.spotter_agents = []
-        for i in range(len(self.agent_list)):
-            agent_env = copy.deepcopy(self.env)
-            spotter_agent = Spotter_Agent(self.agent_list[i], self.function_handle_list[i], agent_env, init_obs)
-            self.spotter_agents.append(spotter_agent)
+        for agent in self.spotter_agents:
+            agent.ep_reward = 0
+            agent.is_done_ep = False
+            agent.env = copy.deepcopy(self.env)
+            agent.obs = init_obs
+            agent.last_frame = None
 
     """run for number of episode"""
     def run(self, num_eps=10):
@@ -59,10 +70,11 @@ class Spotter:
         for _ in range(num_eps):
             self.reset_agents()
             self.run_episode()
+            self.save_run_hist()
 
     """per episode run"""
     def run_episode(self):
-        for i in range(self.ep_max_length+1):
+        for i in range(self.ep_max_length):
             done_counter = 0
             for k, agent in enumerate(self.spotter_agents):
                 if not agent.is_done_ep:
@@ -74,11 +86,17 @@ class Spotter:
             if done_counter == len(self.spotter_agents):
                 break
 
+    """save the episode reward in the agent's buffer"""
+    def save_run_hist(self):
+        for agent in self.spotter_agents:
+            agent.ep_reward_hist.append(copy.deepcopy(agent.ep_reward))
+
 
 """Helper function to run one step of the simulation"""
 
+
 def run_agent_ep(agent):
-    output = agent.env.render(mode="rgb_array")
+    agent.last_frame = agent.env.render(mode="rgb_array")
     action = agent.take_action(agent.obs)
     obs2, reward, done, _ = agent.env.step(action)
     agent.ep_reward += reward
@@ -86,4 +104,3 @@ def run_agent_ep(agent):
     agent.is_done_ep = done
     if done:
         agent.env.close()
-
